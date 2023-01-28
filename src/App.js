3b09442b86f88a31d9f1ee4421d223c341a6d06d
Maya Harris <72241568+mjoyharris@users.noreply.github.com>
2023-01-28 01:17:56 -0500

    fixed merge conflicts

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header">D
         <img src={gatorLogo} className="App-logo" alt="logo" />
         <p> Hello World! </p>
         <p> My name is Maya and this is my project </p>
+        <img src={gatorLogo} className="App-logo" alt="logo" />
+        <p>
+          Hello World! This is our team project.
+        </p>	
       </header>
     </div>
   );
